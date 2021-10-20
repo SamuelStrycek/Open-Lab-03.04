c4bb3ceae8e7db09745a47288dd197b3008171b4
SamuelStrycek <91180378+SamuelStrycek@users.noreply.github.com>
2021-10-20 23:21:13 +0200

    Add files via upload

---
+++ Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Open_Lab_03._04
+{
+    public class Numbers
+    {
+        public string EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "even";
+            }
+            else
+            {
+                return "odd";
+            }
+        }
+
+    }
+}
